f9b8cc6edfb1527e9552fb05f64d4bea916f9645
Manthan Mule <mulemanthan525@gmail.com>
2026-01-19 21:36:26 +0530

    Strings Basic codes

---
+++ CLASS/String.js
@@ -0,0 +1,20 @@
+const str1 = "JS";
+console.log(typeof str1); // "string"
+console.log(str1);
+
+//good way to concatenate strings(String Interpolation)
+console.log(`Hello my Course Name is ${str1}`); // Template Literal
+
+const game_count = new String("Manthan"); 
+
+console.log(game_count.length);
+console.log(game_count[4]);
+console.log(game_count.toUpperCase());
+console.log(game_count.toLowerCase());
+console.log(game_count.charAt(3));
+console.log(game_count.indexOf("t"));
+
+const Fname = "  Manthan  ";
+console.log(Fname)
+console.log(Fname.trim());
+console.log(Fname)
--- CLASS/dataType.js
+++ CLASS/dataType.js
@@ -17,12 +17,15 @@ let b = null;
 let big = 9007199254740991n;
 
 // 7. Symbol
-let sym = Symbol("id");
+let sym = Symbol("123");
 
 // 8. Object
 let obj = { name: "Manthan" };
 
-// Output check
+let id = "123";
+console.log(id===a)
+
+
 console.log(typeof num);        // "number"
 console.log(typeof str);        // "string"
 console.log(typeof isTrue);     // "boolean"
@@ -32,4 +35,26 @@ console.log(typeof big);        // "bigint"
 console.log(typeof sym);        // "symbol"
 console.log(typeof obj);        // "object"
 
+
+//Array
+const arr = [1, 2, 3, 4, 5];
+console.log(typeof arr);
+
+const myobj = { name: "Manthan", age: 23};
+console.log(typeof myobj); // object
+console.log(myobj.name); // Manthan
+console.log(myobj.age); // 23
+
+//Function
+function myFunction() {
+    return "Hello, World!";
+}
+console.log(typeof myFunction); // function
+console.log(myFunction()); // Hello, World!
+
+let func =function myname(){
+    return "Manthan";
+}
+console.log(typeof func); // function
+console.log(func()); // Manthan
 
